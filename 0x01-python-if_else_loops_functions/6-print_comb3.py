8962e47667b692cc93b364950f5eaa37fdc55617
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-02 12:13:34 +0000

    Fix pycodestyle issue

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(9):
+for x in range(10):
     for y in range(x, 10):
-        print("{}{}".format(x, y), end="\n" if x == 8 and y == 9 else ", ")
+        print("{:d}{:d}".format(x, y), end="\n" if x == 8 and y == 9 else ", ")
