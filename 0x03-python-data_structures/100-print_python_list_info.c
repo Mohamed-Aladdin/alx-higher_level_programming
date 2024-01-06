ea3094a17b71f4db1f3f7fa17ddac233746cfa1d
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-06 08:40:33 +0000

    Fix py file

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,8 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <Python.h>
 
 /**
  * print_python_list_info - prints some basic info about Python lists.
