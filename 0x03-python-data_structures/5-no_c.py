eb9535b2e987b92c07aa9921206c8682a6ad2796
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-06 07:58:20 +0000

    Fix py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     temp = ""
 
-    for c in my_string:
-        if c != 'C' and c != 'c':
-            temp += c
+    for letter in my_string:
+        if letter != 'C' and letter != 'c':
+            temp += letter
 
         return (temp)
