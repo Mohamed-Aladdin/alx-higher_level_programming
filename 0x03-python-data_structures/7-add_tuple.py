1cff9cce6eb1a32576a2a162e1331d38e25c5879
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-06 08:12:37 +0000

    Fix py file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b0 = 0 if len(tuple_b) < 1 else tuple_b[0]
     b1 = 0 if len(tuple_b) < 2 else tuple_b[1]
 
-    return (tuple_a[0] + tuple_b[0], a1 + b1)
+    return (a0 + b0, a1 + b1)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    if not my_list:
+        return (None)
+    else:
+        max = my_list[0]
 
-    for n in my_list:
-        if n > max:
-            max = n
+        for n in my_list:
+            if n > max:
+                max = n
 
-    return (max)
+        return (max)
