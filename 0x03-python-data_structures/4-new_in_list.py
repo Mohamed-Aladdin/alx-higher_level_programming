4e7108dff5aec6f29701fd2b376c6ffc19eef405
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-06 07:48:16 +0000

    Fix py file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 def new_in_list(my_list, idx, element):
     temp = my_list.copy()
 
-    if idx >= 0 or idx < len(temp):
+    if idx < 0 or idx >= len(temp):
+        return (temp)
+    else:
         temp[idx] = element
-
-    return (temp)
+        return (temp)
