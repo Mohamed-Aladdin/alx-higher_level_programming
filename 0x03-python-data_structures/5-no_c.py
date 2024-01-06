e7450e1ed5dee48a8a026cc036935dc7752f1ec1
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-06 07:59:32 +0000

    Fix py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    temp = ""
-
+    temp = ''
     for letter in my_string:
         if letter != 'C' and letter != 'c':
             temp += letter
-
         return (temp)
