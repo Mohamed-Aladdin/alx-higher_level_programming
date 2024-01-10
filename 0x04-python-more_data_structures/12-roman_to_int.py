0f90d153f2e2ff42d1a6a790a4d43e7735ca7eb2
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-10 13:58:01 +0000

    Fix py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string or type(roman_string) is not str:
         return (0)
     t = 0
     d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
