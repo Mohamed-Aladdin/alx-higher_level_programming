fceb58c07532dc55ae569138c1c7a7c4471f3825
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-22 10:43:33 +0000

    Fix py file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             elements += 1
         except IndexError:
             break
-        print("")
+    print("")
 
-        return elements
+    return elements
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except as Exception:
+    except as (ValueError, TypeError):
         return False
     else:
         return True
