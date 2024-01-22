3b8f14f3fe26a74edca94a26877e4a4867301e83
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-22 09:52:47 +0000

    Initial commit

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as code:
+        print("Exception: {}".format(code), file=sys.stderr)
+        return None
---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    value = 0
+
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception("Too far")
+            else:
+                value += (a ** b) / x
+        except:
+            value = a + b
+            break
+
+    return value
