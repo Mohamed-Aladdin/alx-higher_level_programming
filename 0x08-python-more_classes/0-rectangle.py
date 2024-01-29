da3719e543faa26459153d121796a7bdc8b41fd1
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-29 10:08:24 +0000

    Add mandatory tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""My Rectangle module"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is ALX's higher-level programming. Python - More Classes and Objects.
