b2a3f8113cb081a7c86a0ba5b28b4cdfb283253e
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-04-26 11:28:19 +0000

    Add mandatory and advanced tasks

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+curl -s -L -o /dev/null -s -w "%{http_code}" "$1"
---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
