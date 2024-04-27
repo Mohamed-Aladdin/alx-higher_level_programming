d7f00c7223cf1419aa4b99ef9bff2f40fb9fa928
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-04-27 13:09:46 +0000

    Fix script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request as request
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as result:
-        body = result.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
-- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
