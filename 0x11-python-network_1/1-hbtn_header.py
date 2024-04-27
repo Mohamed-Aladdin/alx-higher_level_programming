0d654159b9f9f60213dc61c733edcfce8a0d38cd
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-04-27 13:12:04 +0000

    Fix script

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request as request
 import sys
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen(sys.argv[1]) as result:
-        info = result.info()
-        print(info.get("X-Request-Id"))
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
